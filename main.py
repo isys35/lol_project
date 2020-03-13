9454ecff89de5f99c571ee4f4cd80ee2989859e1
isys35 <isys35@mail.ru>
2020-03-13 17:02:20 +0300

    Добавление интерфейса в парсер

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import vilkawidget
 import time
 import async_request
 
+
 def transform_name(events, key):
     names = []
     for event in events:
@@ -40,36 +41,56 @@ class MainApp(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
         self.find_same_matches = ThreadParser(self)
         self.threadparserodds = ThreadParserOdds(self)
         self.same_matches = []
-        self.vilka_wigets = []
+        self.same_matches_objects = []
+        self.vilka_widgets = []
+        self.vilka_data = []
 
     def start_find_same_matches(self):
         self.find_same_matches.start()
         self.threadparserodds.start()
 
     def update_vilka_widgets(self):
+        for game in self.vilka_data:
+            v = VilkaWidget(
+                    game[0],
+                    game[1],
+                    game[2],
+                    game[3],
+                    game[4],
+                    game[5],
+                    game[6]
+                )
+            self.vilka_widgets.append(v)
+            self.vilka_data.remove(game)
+            self.verticalLayout_5.addWidget(v)
+
+
+    def update_same_matches(self):
         if not self.same_matches:
             return
-        if not self.vilka_wigets:
+        if not self.same_matches_objects:
             for same_match in self.same_matches:
-                 vilka_widget = VilkaWidget([match['href'] for match in same_match],
+                match_object = SameGame([match['href'] for match in same_match],
                                             [match['champ'] for match in same_match],
-                                            [[match['command1'], match['command2']] for match in same_match])
-                 self.vilka_wigets.append(vilka_widget)
-                 self.verticalLayout_5.addWidget(vilka_widget)
-        print(self.vilka_wigets)
-
-
+                                            [[match['command1'], match['command2']] for match in same_match],
+                                            self)
+                self.same_matches_objects.append(match_object)
+            print(self.same_matches_objects)
 
 
 class VilkaWidget(QtWidgets.QWidget, vilkawidget.Ui_Form):
-    def __init__(self, hrefs, champs, commands):
+    def __init__(self,hrefs, champs, commands, point, k_p, k_x, d):
         super().__init__()
         self.setupUi(self)
         self.hrefs = hrefs
         self.champs = champs
         self.commands = commands
-        self.value = []
+        self.point = point
+        self.k_p = k_p
+        self.k_x = k_x
+        self.d = d
         self.update_main_labels()
+        self.update_odds_labels()
 
     def update_main_labels(self):
         self.label_7.setText(self.commands[0][0] + ' - ' + self.commands[0][1])
@@ -77,49 +98,92 @@ class VilkaWidget(QtWidgets.QWidget, vilkawidget.Ui_Form):
         self.label_8.setText(self.champs[0])
         self.label_10.setText(self.champs[1])
 
-    def update_value(self):
-        if self.value:
-            if not self.value[0] or not self.value[1]:
-                return
-            print(self.value)
-            t_points_pari = []
-            t_points_xbet = []
-            for bet in self.value[0]['total_total']['more']:
-                t_points_pari.append(bet['points'])
-            for bet in self.value[1]['total_total']['more']:
-                t_points_xbet.append(bet['points'])
-            t_tootal_point = []
-            print(t_points_pari)
-            print(t_points_xbet)
-            for point in t_points_pari:
-                if point in t_points_xbet:
-                    t_tootal_point.append(point)
-            t_koef_pari = []
-            t_koef_xbet = []
-            if t_tootal_point:
-                print(t_tootal_point)
-                for point in t_tootal_point:
-                    for bet in self.value[0]['total_total']['more']:
-                        if bet['points'] == point:
-                            t_koef_pari.append(bet['coef'])
-                            break
-                    for bet in self.value[1]['total_total']['smaller']:
-                        if bet['points'] == point:
-                            t_koef_xbet.append(bet['coef'])
-                            break
-                vilki = []
-                for i in range(0,len(t_koef_pari)):
-                    vilka = 1/t_koef_pari[i] + 1/t_koef_xbet[i]
-                    vilki.append(vilka)
-                d = [100*(vilka-1) for vilka in vilki]
-                self.label_2.setText(str(round(d[0],2)))
-                self.label_11.setText('Т больше {}'.format(t_tootal_point[0]))
-                self.label_12.setText('Т меньше {}'.format(t_tootal_point[0]))
-                self.label_15.setText('{}'.format(t_koef_pari[0]))
-                self.label_16.setText('{}'.format(t_koef_xbet[0]))
+    def update_odds_labels(self):
+        self.label_2.setText(str(round(self.d,2)))
+        self.label_11.setText(str(self.point))
+        self.label_12.setText(str(self.point))
+        self.label_15.setText(str(self.k_p))
+        self.label_16.setText(str(self.k_x))
 
 
+class SameGame:
+    def __init__(self, hrefs, champs, commands, window):
+        super().__init__()
+        self.hrefs = hrefs
+        self.champs = champs
+        self.commands = commands
+        self.value = []
+        self.window = window
 
+    def update_vilki(self):
+        print('Обновление вилок')
+        if not self.value:
+            return
+        if not self.value[0] or not self.value[1]:
+            return
+        print(self.value)
+        t_points_pari = []
+        t_points_xbet = []
+        for bet in self.value[0]['total_total']['more']:
+            t_points_pari.append(bet['points'])
+        for bet in self.value[1]['total_total']['more']:
+            t_points_xbet.append(bet['points'])
+        t_tootal_point = []
+        print(t_points_pari)
+        print(t_points_xbet)
+        for point in t_points_pari:
+            if point in t_points_xbet:
+                t_tootal_point.append(point)
+        t_koef_pari = []
+        t_koef_xbet = []
+        t_vilki = []
+        if t_tootal_point:
+            print(t_tootal_point)
+            for point in t_tootal_point:
+                for bet in self.value[0]['total_total']['more']:
+                    if bet['points'] == point:
+                        t_koef_pari.append(bet['coef'])
+                        break
+                for bet in self.value[1]['total_total']['smaller']:
+                    if bet['points'] == point:
+                        t_koef_xbet.append(bet['coef'])
+                        break
+            for i in range(0,len(t_koef_pari)):
+                vilka = 1/t_koef_pari[i] + 1/t_koef_xbet[i]
+                t_vilki.append(vilka)
+        print(t_koef_pari)
+        print(t_koef_xbet)
+        print(t_vilki)
+        d = [100*(1-vilka) for vilka in t_vilki]
+        print(d)
+        self.window.vilka_data = []
+        for i in range(0, len(d)):
+            self.window.vilka_data.append([self.hrefs,
+                               self.champs,
+                               self.commands,
+                               t_tootal_point[i],
+                               t_koef_pari[i],
+                               t_koef_xbet[i],
+                               d[i]])
+        for game in self.window.vilka_data:
+            for vilka in self.window.vilka_widgets:
+                if not False in [game[0] == vilka.hrefs,
+                                 game[1] == vilka.champs,
+                                 game[2] == vilka.commands,
+                                 game[3] == vilka.point]:
+                    vilka.k_p = game[4]
+                    vilka.k_x = game[5]
+                    vilka.d = game[6]
+                    vilka.update_odds_labels()
+                    self.window.vilka_data.remove(game)
+        if self.window.vilka_data:
+            print(self.window.vilka_data)
+            print('CLICK')
+            try:
+                self.window.pushButton_2.click()
+            except Exception as ex:
+                print(ex)
+                print(traceback.format_exc())
 
 class ThreadParser(QThread):
     def __init__(self, window):
@@ -156,6 +220,8 @@ class ThreadParser(QThread):
         while True:
             events = self.get_events()
             self.window.same_matches = self.search_matches(events)
+            if self.window.same_matches:
+                self.window.update_same_matches()
 
 
 class ThreadParserOdds(QThread):
@@ -168,17 +234,16 @@ class ThreadParserOdds(QThread):
         heads_pari = []
         urls_xbet = []
         heads_xbet = []
-        vilkawidgets = self.window.vilka_wigets
+        same_matches_objects = self.window.same_matches_objects
         hrefs_pari = []
         hrefs_xbet = []
-        print(len(self.window.vilka_wigets))
-        for wigdet in vilkawidgets:
-            hrefs_pari.append(wigdet.hrefs[0])
-            url_p, head_p =self.window.parimatch.get_request_value(wigdet.hrefs[0])
+        for same_match_object in same_matches_objects:
+            hrefs_pari.append(same_match_object.hrefs[0])
+            url_p, head_p =self.window.parimatch.get_request_value(same_match_object.hrefs[0])
             urls_pari.append(url_p)
             heads_pari.append(head_p)
-            hrefs_xbet.append(wigdet.hrefs[1])
-            url_x, head_x = self.window.xbet.get_request_value(wigdet.hrefs[1])
+            hrefs_xbet.append(same_match_object.hrefs[1])
+            url_x, head_x = self.window.xbet.get_request_value(same_match_object.hrefs[1])
             urls_xbet.append(url_x)
             heads_xbet.append(head_x)
         urls_pari.extend(urls_xbet)
@@ -207,17 +272,17 @@ class ThreadParserOdds(QThread):
         value_pari.extend(value_xbet)
         print('value_pari')
         print(value_pari)
-        for widget in self.window.vilka_wigets:
-            widget.value = []
-            for href in widget.hrefs:
+        for same_match_object in self.window.same_matches_objects:
+            same_match_object.value = []
+            for href in same_match_object.hrefs:
                 if href in hrefs_pari:
                     print('True')
-                    widget.value.append(value_pari[hrefs_pari.index(href)])
-            widget.update_value()
+                    same_match_object.value.append(value_pari[hrefs_pari.index(href)])
+            same_match_object.update_vilki()
 
     def run(self):
         while True:
-            if self.window.vilka_wigets:
+            if self.window.same_matches_objects:
                 time.sleep(1)
                 try:
                     self.get_odds()
